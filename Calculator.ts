9c481001ce0a03c40cda3e60f2620a8c7f3afe12
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-08 18:08:11 +0530

    function with params type & return type , void, any, string, number, boolean

--- Calculator.ts
+++ Calculator.ts
@@ -1,18 +1,37 @@
+// Primitive types -> 
+let a:any;
+a=12121;
+console.log(typeof a); //-> number
+
+let b:any;
+console.log(typeof b); //-> undefined
+b=3043;
+b="dffdf";
+console.log(typeof b); // -> string
+
+// Function -> Simple calculator - Mini-Project
 function add (x:number, y:number):number{
     return x+y;
-}
+}    
 
 const mul = (x:number, y:number):number => x*y
 
 const divide = (x:number, y:number):number =>{
-    if(x<0)return 0
+    if(x<=0)return 0
     return x/y
-}
+}    
 
 const substract = (x:number, y:number):number => x-y;
 
- function result(x:Function,a:number, b:number):void{
+function result(x:Function,a:number, b:number):void{
     console.log(x(a,b));
-}
+}    
 
-result(add,1,0);
+result(add,1,20); //   -> 21
+result(substract,25,4); //   -> 21
+result(mul,7,3); //   -> 21
+result(divide,21,3); //   -> 7
+
+
+
+
