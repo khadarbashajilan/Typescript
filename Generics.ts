acec09e25ed8c04556350ea22a300073d76fc512
M KHADAR BASHA JILAN <khadarbashajilan001@gmail.com>
2025-07-11 18:58:50 +0530

    Generic (classes, interface and function) in TS

--- Generics.ts
+++ Generics.ts
@@ -1,5 +1,5 @@
 {
-    // Generics
+    // Generic Functions
     function PrinT<T>(x:T):T{
         return x;
     }
@@ -8,3 +8,35 @@
     console.log(PrinT(true));
 }
 
+{
+    // Generic Interface
+    interface KeyVal <T, U>{
+        name:T;
+        id:U;
+    }
+
+    class Unknown implements KeyVal <string, number> {
+
+        constructor(public name:string, public id:number ){
+            this.id=id;
+            this.name=name;
+        }
+    }
+
+
+
+    const U1= new Unknown("hi",122);
+    console.log(U1);
+
+
+}
+
+{
+    // Generic Classes
+    class Store <T>{
+        constructor(public data:T){};
+    }
+
+    const S1 = new Store("Something");
+    console.log(S1);
+}
